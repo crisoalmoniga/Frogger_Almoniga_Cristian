663a8233ab9bf0d746a416d7cc69f59d8c90308d
crisoalmoniga <cristian.almoniga@gmail.com>
2024-01-09 21:01:14 -0300

    Salud y corazones

--- Frogger_Almoniga_cristian.cpp
+++ Frogger_Almoniga_cristian.cpp
@@ -10,6 +10,8 @@
 
 using namespace std;
 
+int corazones = 3;  // Agrega esta variable global para contar los corazones
+
 // Función para posicionar el cursor en la consola
 void irAxy(int x, int y) {
 	HANDLE hCon;
@@ -61,8 +63,21 @@ void setColorTexto(int color) {
 	SetConsoleTextAttribute(hConsole, color);
 }
 
-// Función para dibujar una rana en la consola
-void dibujarRana(int x, int y) {
+// Clase Rana
+class Rana {
+	int x, y;
+	
+public:
+	Rana(int _x, int _y);
+	void dibujar();
+	void borrar();
+	void mover(int direccion);
+	void pintar_corazones();
+};
+
+Rana::Rana(int _x, int _y) : x(_x), y(_y) {}
+
+void Rana::dibujar() {
 	// Establece el color de fondo a verde y el color del texto a negro
 	setColorTexto(FOREGROUND_GREEN);
 	
@@ -77,31 +92,80 @@ void dibujarRana(int x, int y) {
 	setColorTexto(FOREGROUND_GREEN | FOREGROUND_BLUE | FOREGROUND_RED);
 }
 
-class AUTO {
+void Rana::borrar() {
+	irAxy(x, y);
+	printf("       ");  // Cuerpo superior de la rana
+	irAxy(x, y + 2);
+	printf("       ");  // Ojos de la rana
+	irAxy(x, y + 3);
+	printf("         ");  // Cuerpo inferior de la rana
+}
+
+void Rana::mover(int direccion) {
+	borrar();
+	
+	// Actualiza las coordenadas según la dirección
+	switch (direccion) {
+	case IZQUIERDA:
+		if (x > 1)
+			x--;
+		break;
+	case DERECHA:
+		if (x + 1 > 18)
+			x++;
+		break;
+	case ARRIBA:
+		if (y > 4)
+			y--;
+		break;
+	case ABAJO:
+		if (y + 2 < 23)
+			y++;
+		break;
+	}
+	
+	pintar_corazones();
+	
+	dibujar();
+}
+
+void Rana::pintar_corazones() {
+	gotoxy(64, 2);
+	printf("Salud");
+	gotoxy(70, 2);
+	printf("     ");
+	for (int i = 0; i < corazones; i++) {
+		gotoxy(70 + i, 2);
+		printf("%c", 3);
+	}
+}
+
+// Clase Auto
+class Auto {
 	int x, y;
-	int direccion;  // Variable para almacenar la dirección del movimiento
+	int direccion;
 	
 public:
-	AUTO(int _x, int _y, int _direccion);
+	Auto(int _x, int _y, int _direccion);
 	void pintar();
 	void borrar();
 	void mover();
 	void reiniciarPosicion();
 };
 
-AUTO::AUTO(int _x, int _y, int _direccion) : x(_x), y(_y), direccion(_direccion) {}
+Auto::Auto(int _x, int _y, int _direccion) : x(_x), y(_y), direccion(_direccion) {}
 
-void AUTO::pintar() {
+void Auto::pintar() {
 	irAxy(x, y);
 	printf("%c%c%c%c%c", 219, 219, 219, 219, 219); // Cuadrado grande
 }
 
-void AUTO::borrar() {
+void Auto::borrar() {
 	irAxy(x, y);
 	printf("                                                                                    "); // Espacios para borrar el cuadrado grande
 }
 
-void AUTO::mover() {
+void Auto::mover() {
 	borrar();
 	
 	// Mover hacia la izquierda
@@ -116,7 +180,7 @@ void AUTO::mover() {
 	pintar();
 }
 
-void AUTO::reiniciarPosicion() {
+void Auto::reiniciarPosicion() {
 	// Reiniciar la posición cuando alcanza el borde de la pantalla
 	if (direccion == 1 && x <= 1) {
 		x = 80;
@@ -128,15 +192,16 @@ void AUTO::reiniciarPosicion() {
 int main() {
 	OcultarCursor();
 	// Establece las coordenadas iniciales para la rana
-	int xRana = 55, yRana = 27;
+	Rana rana(55, 27);
 	pintar_limites();
+	rana.pintar_corazones();
 	
 	// Establece las coordenadas iniciales y direcciones para los autos
-	AUTO autoIzquierda(2, 10, 1);  // Dirección: Izquierda
-	AUTO autoDerecha(80, 18, 2);    // Dirección: Derecha
+	Auto autoIzquierda(2, 10, 1);  // Dirección: Izquierda
+	Auto autoDerecha(80, 18, 2);    // Dirección: Derecha
 	
 	// Ajusta la velocidad del movimiento
-	int velocidadDesplazamiento = 2; // Puedes cambiar este valor según tu preferencia
+	int velocidadDesplazamiento = 3; // Puedes cambiar este valor según tu preferencia
 	
 	bool game_over = false;
 	while (!game_over) {
@@ -144,18 +209,23 @@ int main() {
 			char tecla = _getch();
 			
 			// Borra la rana en la posición anterior
-			irAxy(xRana, yRana);
-			printf("       ");  // Cuerpo superior de la rana
-			irAxy(xRana, yRana + 2);
-			printf("       ");  // Ojos de la rana
-			irAxy(xRana, yRana + 3);
-			printf("         ");  // Cuerpo inferior de la rana
+			rana.borrar();
 			
 			// Actualiza las coordenadas según la tecla presionada
-			if (tecla == DERECHA && xRana + 1 > 18) xRana += velocidadDesplazamiento;
-			if (tecla == IZQUIERDA && xRana > 1) xRana -= velocidadDesplazamiento;
-			if (tecla == ARRIBA && yRana > 4) yRana -= velocidadDesplazamiento;
-			if (tecla == ABAJO && yRana + 2 < 23) yRana += velocidadDesplazamiento;
+			switch (tecla) {
+			case DERECHA:
+				rana.mover(DERECHA);
+				break;
+			case IZQUIERDA:
+				rana.mover(IZQUIERDA);
+				break;
+			case ARRIBA:
+				rana.mover(ARRIBA);
+				break;
+			case ABAJO:
+				rana.mover(ABAJO);
+				break;
+			}
 		}
 		
 		// Mueve los autos
@@ -167,7 +237,7 @@ int main() {
 		autoDerecha.reiniciarPosicion();
 		
 		// Dibuja la rana en las nuevas coordenadas
-		dibujarRana(xRana, yRana);
+		rana.dibujar();
 		
 		// Añade un pequeño retardo para que el usuario pueda ver el resultado y interactuar
 		Sleep(30);
